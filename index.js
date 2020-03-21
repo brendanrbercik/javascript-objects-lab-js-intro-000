0f34d2e0e30b0e6aaec9962ce42d140cfb1e6b6a
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-21 23:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {bananas: "1", strawberries: "3"};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},object, {[key]:value});
 }
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
